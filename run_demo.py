c7ffe2b537fab4b052d31f80294ef5889a11eb34
dannyway03 <dannyway03@gmail.com>
2025-01-14 08:17:58 +0100

    Fixed pth demo sample.

--- run_demo.py
+++ run_demo.py
@@ -1,6 +1,7 @@
 import argparse
 import glob
 import os
+import time
 import warnings
 
 from PIL import Image
@@ -17,19 +18,14 @@ def get_args_parser():
     # * Backbone
     parser.add_argument('--backbone', default='vgg16_bn', type=str,
                         help="name of the convolutional backbone to use")
-
     parser.add_argument('--row', default=2, type=int,
                         help="row number of anchor points")
     parser.add_argument('--line', default=2, type=int,
                         help="line number of anchor points")
-
-    parser.add_argument('--output_dir', default='',
-                        help='path where to save')
-    parser.add_argument('--weight_path', default='',
-                        help='path where the trained weights saved')
-
-    parser.add_argument('--gpu_id', default=0, type=int, help='the gpu used for evaluation')
-
+    parser.add_argument('--weights', default='/home/nicola/Software/CrowdCounting-P2PNet/weights/SHTechA.pth',
+                        help='path .pth weights')
+    parser.add_argument('--input_video', default='/home/nicola/Software/CrowdCounting-P2PNet/testData/demo.mp4',
+                        help='path to input video')
     return parser
 
 
@@ -49,7 +45,6 @@ def Predict(model, img_path, transform, device):
     # run inference
     outputs = model(samples)
     outputs_scores = torch.nn.functional.softmax(outputs['pred_logits'], -1)[:, :, 1][0]
-
     outputs_points = outputs['pred_points'][0]
 
     threshold = 0.5
@@ -57,9 +52,6 @@ def Predict(model, img_path, transform, device):
     points = outputs_points[outputs_scores > threshold].detach().cpu().numpy().tolist()
     predict_cnt = int((outputs_scores > threshold).sum())
 
-    outputs_scores = torch.nn.functional.softmax(outputs['pred_logits'], -1)[:, :, 1][0]
-
-    outputs_points = outputs['pred_points'][0]
     # draw the predictions
     size = 4
     img_to_draw = cv2.cvtColor(np.array(img_raw), cv2.COLOR_RGB2BGR)
@@ -74,52 +66,89 @@ def Predict(model, img_path, transform, device):
     return img_to_draw
 
 
-def main(args, debug=False):
-    os.environ["CUDA_VISIBLE_DEVICES"] = '{}'.format(args.gpu_id)
+def process_frame(samples, model):
+    outputs = model(samples)
+    outputs_scores = torch.nn.functional.softmax(outputs['pred_logits'], -1)[:, :, 1][0]
+    outputs_points = outputs['pred_points'][0]
+    threshold = 0.5
+    # filter the predictions
+    points = outputs_points[outputs_scores > threshold].detach().cpu().numpy().tolist()
+    predict_cnt = int((outputs_scores > threshold).sum())
 
-    print(args)
-    device = torch.device('cuda')
-    # get the P2PNet
-    model = build_model(args)
-    # move to GPU
-    model.to(device)
-    # load trained model
-    if args.weight_path is not None:
-        checkpoint = torch.load(args.weight_path, map_location='cpu')
+    return points, predict_cnt
+
+
+def main(args):
+    print('Building model:')
+    print('   backbone: %s' % args.backbone)
+    print('   anchor cols x rows: %s x %s' % (args.line, args.row))
+    model = build_model(args).to('cpu')
+
+    print('Loading model weights: %s' % args.weights)
+    if args.weights is not None:
+        checkpoint = torch.load(args.weights, map_location='cpu')
         model.load_state_dict(checkpoint['model'])
-    # convert to eval mode
+    else:
+        print('Loading checkpoint failed')
+        exit(1)
+
+    print('Setting model in eval mode')
     model.eval()
+
     # create the pre-processing transform
     transform = standard_transforms.Compose([
         standard_transforms.ToTensor(),
         standard_transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
-    # 获取文件夹下的 JPG 图像
-    img_dir = '/mnt/d/MyDocs/Datasets/mall_dataset/frames'
-    img_files = glob.glob(f'{img_dir}/*.jpg')
+    w, h = 1280, 768
 
-    # 设置视频编解码器和帧率
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    fps = 30.0
+    cap = cv2.VideoCapture(args.input_video)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
 
-    # 创建视频写入器对象
-    width, height = Predict(model, img_files[0], transform, device).shape[:2]
-    video_out = cv2.VideoWriter('predict.mp4', fourcc, fps, (height, width))
+    width = width // 128 * 128
+    height = height // 128 * 128
 
-    for img_file in img_files:
-        processed_image = Predict(model, img_file, transform, device)
-        video_out.write(processed_image)
+    w = min(w, width)
+    h = min(h, height)
+
+    video_out = cv2.VideoWriter('./out.mp4', cv2.VideoWriter.fourcc(*'mp4v'), fps, (w, h))
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            t1 = time.time()
+            frame = cv2.resize(frame, (w, h))
+            frame_ = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            img = transform(frame_)
+            samples = torch.Tensor(img).unsqueeze(0)
+            points, count = process_frame(samples, model)
+
+            t2 = time.time()
+            print('time: {}'.format(t2 - t1), flush=True)
+
+            # draw the predictions
+            img_to_draw = cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR)
+            cv2.putText(img_to_draw, 'Count: %d' % count, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2,
+                        cv2.LINE_AA)
+
+            for p in points:
+                img_to_draw = cv2.circle(img_to_draw, (int(p[0]), int(p[1])), 2, (0, 0, 255), -1)
+
+            cv2.imshow('result', img_to_draw)
+            video_out.write(img_to_draw)
+            cv2.waitKey(1)
 
     video_out.release()
     cv2.destroyAllWindows()
-    print('生成成功')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('P2PNet evaluation script', parents=[get_args_parser()])
-    args = parser.parse_args()
-    main(args)
+    main(parser.parse_args())
 
 # import cv2
 # import matplotlib.pyplot as plt
